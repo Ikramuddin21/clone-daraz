15f133ee38478988ea46c3100d4d6c52d7840b9b
Ikramuddin21 <shaonakhand2122@gmail.com>
2023-09-19 20:02:52 +0600

    all url updated

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -1,23 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Category = () => {
-    const [categories, setCategories] = useState([]);
-    const { category } = useParams();
+  const [categories, setCategories] = useState([]);
+  const { category } = useParams();
 
-    useEffect(() => {
-        const fetchCategory = async () => {
-            const res = await fetch(`http://localhost:5000/products/${category}`);
-            const data = await res.json();
-            console.log(data);
-        };
-        fetchCategory();
-    }, [category]);
-    return (
-        <div>
-
-        </div>
-    );
+  useEffect(() => {
+    const fetchCategory = async () => {
+      const res = await fetch(
+        `https://clone-daraz-server.onrender.com/products/${category}`
+      );
+      const data = await res.json();
+      console.log(data);
+    };
+    fetchCategory();
+  }, [category]);
+  return <div></div>;
 };
 
 export default Category;
--- src/Pages/Home/Shop/Shop.js
+++ src/Pages/Home/Shop/Shop.js
@@ -1,52 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Products from '../Products/Products';
-import './Shop.css';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Products from "../Products/Products";
+import "./Shop.css";
 
 const Shop = () => {
-    const initialProduct = 24;
-    const [products, setProducts] = useState([]);
-    const [loadProduct, setLoadProduct] = useState(initialProduct);
-    const navigate = useNavigate();
+  const initialProduct = 24;
+  const [products, setProducts] = useState([]);
+  const [loadProduct, setLoadProduct] = useState(initialProduct);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await fetch("http://localhost:5000/products");
-            const data = await res.json();
-            setProducts(data);
-        };
-        fetchProducts();
-    }, []);
-
-    // load more products
-    const handleLoadMore = () => {
-        setLoadProduct(loadProduct + initialProduct);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch(
+        "https://clone-daraz-server.onrender.com/products"
+      );
+      const data = await res.json();
+      setProducts(data);
     };
+    fetchProducts();
+  }, []);
 
-    // handle products details
-    const handleProductDetails = id => {
-        navigate(`/productDetails/${id}`);
-    };
+  // load more products
+  const handleLoadMore = () => {
+    setLoadProduct(loadProduct + initialProduct);
+  };
 
-    return (
-        <div className="shop-container">
-            <h2 className="shop-title">Just For You</h2>
-            <div className="products">
-                {
-                    products?.slice(0, loadProduct).map(product => <Products 
-                        key={product.key} 
-                        product={product} 
-                        handleProductDetails={handleProductDetails}
-                        />)
-                }
-            </div>
-            {
-                loadProduct < products.length && <div className="product-load-more">
-                    <button onClick={handleLoadMore} className="load-more-btn highlight-color">Load More</button>
-                </div>
-            }
+  // handle products details
+  const handleProductDetails = (id) => {
+    navigate(`/productDetails/${id}`);
+  };
+
+  return (
+    <div className="shop-container">
+      <h2 className="shop-title">Just For You</h2>
+      <div className="products">
+        {products?.slice(0, loadProduct).map((product) => (
+          <Products
+            key={product.key}
+            product={product}
+            handleProductDetails={handleProductDetails}
+          />
+        ))}
+      </div>
+      {loadProduct < products.length && (
+        <div className="product-load-more">
+          <button
+            onClick={handleLoadMore}
+            className="load-more-btn highlight-color"
+          >
+            Load More
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Shop;
